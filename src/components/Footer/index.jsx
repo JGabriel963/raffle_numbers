cb9033f045810f0bbcfe7f7a26e1fc2c28254b80
João Gabriel <joaogabriel9633@gmail.com>
2023-07-11 18:59:52 -0300

    Icon update to Mobile

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
                 />
             ) : (
                 <FaRegHeart 
-                    className='text-red mx-2 cursor-pointer transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 '
+                    className='text-red mx-2 md:cursor-pointer md:transition md:ease-in-out md:delay-150 md:bg-blue-500  md:hover:-translate-y-1 md:hover:scale-110 md:hover:bg-indigo-500 md:duration-300 '
                     onClick={() => setHeart(!heart)}  
                 /> 
             )} por João Gabriel
